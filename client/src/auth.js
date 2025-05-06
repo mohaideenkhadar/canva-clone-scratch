6b5096cefdb72ddcec8abb93a782ca82a2311e6e
mohaideenkhadar <mohaideenabdulkhadar006@gmail.com>
2025-05-06 12:30:10 +0530

    Add some changes

--- client/src/auth.js
+++ client/src/auth.js
@@ -1,8 +1,22 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers : [Google],
+    providers : [
+        GoogleProvider({
+            clientId : process.env.AUTH_GOOGLE_ID,
+            clientSecret : process.env.AUTH_GOOGLE_SECRET,
+            authorization: {
+                params : {
+                    prompt : "consent",
+                    access_type : "offline",
+                    response_type : "code"
+                }
+            }
+        })
+    ],
+    secret : process.env.AUTH_SECRET,
+    debug : true,
     callbacks: {
         async jwt({token, account}){
             if(account?.id_token){
--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://api-gateway-7icg.onrender.com/";
+export const API_URL = "https://api-gateway-7icg.onrender.com";
